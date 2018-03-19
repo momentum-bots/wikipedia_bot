e0f588f5d2af98ac41f975d7a3381e9f9f1fd3f8
qwerty432 <dimichkit@ukr.net>
2018-03-19 20:41:33 +0200

    updated text message handler for testing

--- bot_handlers.py
+++ bot_handlers.py
@@ -1,4 +1,6 @@
 from bot import bot
+import wikipedia
+import generate_telegraph
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -8,7 +10,11 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    print(message)
+    text = wikipedia.search(message.text)
+    url = 'https://en.wikipedia.org/wiki/' + text[0].replace(' ', '_')
+    print(url)
+    response = generate_telegraph.generate_by_wiki_url(url)
+    bot.send_message(message.chat.id, response)
 
 
 def start_bot():
