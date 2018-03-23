53626d8f7e0be042b75cd736364dae11f1c90cda
qwerty432 <dimichkit@ukr.net>
2018-03-24 00:11:30 +0200

    reduced big articles

--- generate_telegraph.py
+++ generate_telegraph.py
@@ -5,6 +5,18 @@ from telegraph import Telegraph, TelegraphException
 from config import TELEGRAPH_TOKEN
 import lxml
 
+WIKI_URL = 'https://en.wikipedia.org'
+
+
+def make_pretty(content):
+    new_content = BeautifulSoup(content, 'lxml').prettify()
+    start_tags = ['<html>', '<head>', '<body>', '<tt>']
+    end_tags = [tag[0] + '/' + tag[1:] for tag in start_tags]
+
+    for tag in start_tags + end_tags:
+        new_content = new_content.replace(tag, ' ')
+    return new_content
+
 
 def create_instant_view(content, title) :
     telegraph = Telegraph(TELEGRAPH_TOKEN)
@@ -14,8 +26,9 @@ def create_instant_view(content, title) :
         return response['url']  #url of created telegraph page
 
     except TelegraphException :
+        new_content = "<h".join(content.split('<h')[:-1])
         print('too big article')
-        return "Haha. Loser"
+        return create_instant_view(make_pretty(new_content), title)
 
 
 def generate_by_wiki_url(url):
@@ -43,6 +56,16 @@ def generate_by_wiki_url(url):
             #paragraphs
             elif child.name == 'p':
                 content += '<p>{}</p>'.format(child.get_text())
+                # for p_child in child.children:
+                #     if p_child.name in ['sup', 'span', 'tt']:
+                #         p += p_child.get_text()
+                #     else:
+                #         if p_child.name == 'a':
+                #             p += "<a href='{}'>{}</a>".format(WIKI_URL + p_child['href'], p_child.get_text())
+                #         else:
+                #             p += p_child.string + '&nbsp;'
+                #             print(p_child.string + '&nbsp;')
+                # print(p, '\n')
 
             #lists
             elif child.name in ['ul', 'ol']:
@@ -78,16 +101,12 @@ def generate_by_wiki_url(url):
                         except:
                             print('no image')
 
-    new_content = BeautifulSoup(content, 'lxml').prettify()
-    start_tags = ['<html>', '<head>', '<body>']
-    end_tags = [tag[0] + '/' + tag[1:] for tag in start_tags]
+    new_content = make_pretty(content)
 
-    for tag in start_tags + end_tags:
-        new_content = new_content.replace(tag, '')
-
-    # with open('content.html', 'w') as f:
-    #     f.write(new_content)
+    with open('content.html', 'w') as f:
+        f.write(new_content)
 
     return create_instant_view(new_content, title)
+
 
 # print(generate_by_wiki_url('https://en.wikipedia.org/wiki/Cython'))
