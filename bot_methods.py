33de7e0883c1ae9dfb3b08007793c6b9f2cc1e02
qwerty432 <dimichkit@ukr.net>
2018-03-23 23:22:12 +0200

    added new function for getting article's photo url

--- bot_methods.py
+++ bot_methods.py
@@ -1,8 +1,23 @@
 import wikipedia
 
 
+WIKI_PHOTO = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEaF0CsI_3jjaTkV1Cv3f-WD4evU8EFuqcTUH8_-Fpcrkwo57kQA'
+
+
 def search(text):
     print(wikipedia.search(text))
+
+
+def get_photo_url(article, lang):
+    wikipedia.set_lang(lang)
+    try:
+        page = wikipedia.page(article)
+        images = [image for image in page.images if 'jpg' in image]
+        if images:
+            return images[0]
+    except:
+        pass
+    return WIKI_PHOTO
 
 
 if __name__ == '__main__':
