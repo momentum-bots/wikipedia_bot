ad187a96f28883d688db0c9998232c7782e86a1e
qwerty432 <dimichkit@ukr.net>
2018-03-26 23:08:39 +0300

    updated lang's dict

--- languages.py
+++ languages.py
@@ -8,6 +8,10 @@ LANGUAGES_DICTIONARY = {
                      'uk': 'Мова була успішно оновлена.',
                      'ru': 'Язык был успешно обновлен.'
                     },
+    'wrong_article': {'en': "There's no such article in Wikipedia.. Try again!",
+                      'uk': 'У Вікіпедії немає такої статті. Спробуй знову!',
+                      'ru': 'В Википедии нет такой статьи. Попробуй снова!'
+                     },
     'set_lang': {'en': 'Choose your language:',
                  'uk': 'Виберіть мову:',
                  'ru': 'Выберите язык:'
